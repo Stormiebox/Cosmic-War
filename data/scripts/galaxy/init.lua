3d7da5e17d81baaab1d3e11fb318ce49cbfab367
HazyLunar <erikf08@gmail.com>
2026-06-07 04:21:38 -0700

    Further fixes and updates.

--- data/scripts/galaxy/init.lua
+++ data/scripts/galaxy/init.lua
@@ -1,17 +1,12 @@
-local cw_old_galaxy_init = initialize
-function initialize(...)
-    if cw_old_galaxy_init then cw_old_galaxy_init(...) end
-
-    if onServer() then
-        local galaxy = Galaxy()
-        if galaxy then
-            -- Attach background simulation scripts to the global galaxy loop
-            galaxy:addScriptOnce("data/scripts/server/background/cosmicwarnews.lua")
-            galaxy:addScriptOnce("data/scripts/server/background/cosmicwardiplomaticsanctions.lua")
-            galaxy:addScriptOnce("data/scripts/server/background/cosmicwarceasefires.lua")
-            galaxy:addScriptOnce("data/scripts/server/background/cosmicwarbounties.lua")
-            galaxy:addScriptOnce("data/scripts/server/background/cosmicwarbridgeupdate.lua")
-            galaxy:addScriptOnce("data/scripts/server/background/cosmicwardiplomacy.lua")
-        end
+if onServer() then
+    local galaxy = Galaxy()
+    if galaxy then
+        -- Attach background simulation scripts to the global galaxy loop
+        galaxy:addScriptOnce("data/scripts/server/background/cosmicwarnews.lua")
+        galaxy:addScriptOnce("data/scripts/server/background/cosmicwardiplomaticsanctions.lua")
+        galaxy:addScriptOnce("data/scripts/server/background/cosmicwarceasefires.lua")
+        galaxy:addScriptOnce("data/scripts/server/background/cosmicwarbounties.lua")
+        galaxy:addScriptOnce("data/scripts/server/background/cosmicwarbridgeupdate.lua")
+        galaxy:addScriptOnce("data/scripts/server/background/cosmicwardiplomacy.lua")
     end
 end
